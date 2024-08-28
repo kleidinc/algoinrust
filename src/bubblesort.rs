aafdf1c1f420d6805ae73b38fbfc1068c09b151e
Alex K <kleydints@petalmail.com>
2024-08-28 11:22:11 +0300

    bubblesort works and tested

--- src/bubblesort.rs
+++ src/bubblesort.rs
@@ -1,20 +1,25 @@
-// compare two elements
-// swap the elements if the previous is larger than the following
-// The sorting should be implemented in place
-pub fn bubblesort<T: Ord + Eq>(slice: &mut [T]) {
-    let mut swapped: bool;
-    loop {
-        swapped = false;
-        for i in 0..slice.len() {
-            while i < slice.len() + 1 {
+use super::Sorter;
+
+pub struct BubbleSort;
+
+impl<T: Ord> Sorter<T> for BubbleSort {
+    fn sort(&self, slice: &mut [T]) {
+        let mut swapped = true;
+        while swapped {
+            swapped = false;
+            for i in 0..slice.len() - 1 {
                 if slice[i] > slice[i + 1] {
                     slice.swap(i, i + 1);
                     swapped = true;
                 }
             }
         }
-        if !swapped {
-            break;
-        }
     }
+}
+
+#[test]
+fn bubblesort_works() {
+    let mut things = vec![9, 1, 5, 7];
+    BubbleSort.sort(&mut things);
+    assert_eq!(things, &[1, 5, 7, 9]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
+pub trait Sorter<T> {
+    fn sort(&self, slice: &mut [T])
+    where
+        T: Ord;
+}
+
 mod bubblesort;
 
-#[cfg(test)]
-mod tests {
+pub use bubblesort::BubbleSort;
 
-    #[test]
-    fn it_works() {
-        let mut unsorted = vec![9, 1, 5, 11];
-        crate::bubblesort::bubblesort(&mut unsorted);
-        assert_eq!(&unsorted, &[1, 5, 9, 11]);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//
+//     #[test]
+//     fn it_works() {
+//         let mut unsorted = vec![9, 1, 5, 11];
+//         crate::bubblesort::bubblesort(&mut unsorted);
+//         assert_eq!(&unsorted, &[1, 5, 9, 11]);
+//     }
+// }
